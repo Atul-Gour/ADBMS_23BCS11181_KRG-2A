9178574a9e38c51b690354ffde7c8858e71da322
Atul-Gour <162041144+Atul-Gour@users.noreply.github.com>
2025-07-29 10:57:15 +0530

    Time: 393 ms (56.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select u.name,sum(t.amount) as [BALANCE]
+from Users as u
+inner join
+Transactions as t
+on u.account=t.account
+group by u.name
+having sum(t.amount)>10000
